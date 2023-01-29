2eb6dc01558275ca6b135273cf54b4616e8dabe1
QuittoGames <109162118+QuittoGames@users.noreply.github.com>
2023-01-29 12:21:08 -0300

    Simple_Ccalculator

---
+++ calculadora.py
@@ -0,0 +1,28 @@
+numero = int(input("Digite O 1 numero:"))
+
+numero2 = int(input("Digite O 2 numero:"))
+
+tipodeconta = str(input("Tipo de Calculo:"))
+
+if tipodeconta == "+":
+    print(numero+numero2)
+
+if tipodeconta == "-":
+    print(numero-numero2)
+
+
+if tipodeconta == "*":
+    print(numero*numero2)
+
+if tipodeconta == "/":
+    print(numero/numero2)
+
+if tipodeconta =="**":
+    print(numero**numero2)
+
+if tipodeconta =="//":
+    print(numero//numero2)
+
+
+
+##©Quitto
