3899f67fa7c15e2e5cdd03077831231e8a165c46
QuittoGames <109162118+QuittoGames@users.noreply.github.com>
2023-02-20 10:52:22 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,19 @@
+import math
+
+raiz = bool(input("Voce Quer Fazer Raiz3?:"))
+    
+
+numero3 = int(input("Digite Sua Raiz:"))
+
+if raiz == True:
+    print(math.sqrt(numero3))
+
+
 numero = int(input("Digite O 1 numero:"))
 
 numero2 = int(input("Digite O 2 numero:"))
+
+
 
 tipodeconta = str(input("Tipo de Calculo:"))
 
@@ -18,11 +31,10 @@ if tipodeconta == "/":
     print(numero/numero2)
 
 if tipodeconta =="**":
-    print(numero**numero2)
-
-if tipodeconta =="//":
-    print(numero//numero2)
+    print(math.pow(numero, numero2))
 
 
 
-##©Quitto
+
+
+##ï¿½Quitto
